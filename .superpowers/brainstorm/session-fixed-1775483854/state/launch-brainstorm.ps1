310becf81c2687252740556563326e9c2bfaea24
lqqqqq <3601820158@qq.com>
2026-04-25 10:58:43 +0800

    chore: remove local artifacts from git tracking

--- .superpowers/brainstorm/session-fixed-1775483854/state/launch-brainstorm.ps1
+++
@@ -1,4 +0,0 @@
-﻿$env:BRAINSTORM_DIR = 'D:\code\aaaaljt\.superpowers\brainstorm\session-fixed-1775483854'
-$env:BRAINSTORM_HOST = '127.0.0.1'
-$env:BRAINSTORM_URL_HOST = 'localhost'
-node 'C:\Users\36018\.codex\skills\brainstorming\scripts\server.cjs'
